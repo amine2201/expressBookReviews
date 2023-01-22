1921f53eae4e047c0dfc6f87c1d5e980ca774c98
Amine Jaoui <aminejaoui01@gmail.com>
2023-01-22 15:53:37 +0100

    final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,111 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const password=req.body.password;
+    const username=req.body.username;
+    if(password && username){
+    if(isValid(username)){
+        const user={
+            "username":username,
+            "password":password
+        }
+        users.push(user);
+        res.status(200).send(`user ${username} registered`);
+    }
+    res.status(404).send("username already exists");}
+    else res.status(404).send("username and/or password not valid");
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books,null,4));
 });
-
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books[req.params.isbn],null,4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let booksArray = Object.values(books);
+    res.send(JSON.stringify(booksArray.filter(book => book.author === req.params.author),null,4));
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let booksArray = Object.values(books);
+    res.send(JSON.stringify(booksArray.filter(book => book.title === req.params.title),null,4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const book=books[req.params.isbn];
+    if(book)
+        res.send(JSON.stringify(book.reviews,null,4));
+    else res.send("Book not found");
+});
+//task 10
+function getAllBooks(){
+    return new Promise((resolve,reject)=>{
+        resolve(books);
+    })
+}
+// Get the book list available in the shop async
+public_users.get('/',function (req, res) {
+    getAllBooks()
+    .then(books=>res.send(JSON.stringify(books,null,4)))
+    
+});
+
+//task 11
+function getBookByISBN(isbn){
+    return new Promise((resolve,reject)=>{
+        const book=books[isbn];
+        if(book)
+        resolve(book);
+        else reject("ISBN not found");
+    })
+}
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+    getBookByISBN(req.params.isbn).then(
+        book=>res.send(JSON.stringify(book,null,4)))
+    .catch(err => res.status(404).send(err));
+ });
+
+ //task 12
+ function getBookByAuthor(author){
+     return new Promise((resolve,reject)=>{
+        let booksArray = Object.values(books);
+        const book=booksArray.filter(book => book.author === author);
+        if(book)
+            resolve(book);
+        else reject("author not found");
+     })
+ }
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+    getBookByAuthor(req.params.author)
+    .then(book => res.send(JSON.stringify(book,null,4)))
+    .catch(err => res.status(404).send(err));
+});
+
+//task 12
+function getBookByTitle(title){
+    return new Promise((resolve,reject)=>{
+       let booksArray = Object.values(books);
+       const book=booksArray.filter(book => book.title === title);
+       if(book)
+           resolve(book);
+       else reject("title not found");
+    })
+}
+// Get book details based on author
+public_users.get('/title/:title',function (req, res) {
+   getBookByTitle(req.params.title)
+   .then(book => res.send(JSON.stringify(book,null,4)))
+   .catch(err => res.status(404).send(err));
 });
 
 module.exports.general = public_users;
